3c1d0941d8377e1e1acc19469a2857d85cfa12f8
Caio Carvalho <k4io12345@gmail.com>
2025-11-14 12:12:58 -0300

    helloworld

---
+++ helloworldc.c
@@ -0,0 +1,7 @@
+// importando biblioteca para lidar com inputs e outputs
+#include <stdio.h>
+// definindo função main
+int main(){
+	printf("Hello World C! \n");
+	printf("Meu primeiro programa em C! \n");
+}
